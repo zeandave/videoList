9fe5a881cfd0476062defaad0dde909ad922bd9b
knot <knot@outlook.com>
2018-04-23 23:04:25 +0700

    fixed database2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports={mongoURI:'mongodb://<knot>:<k0874480127>@ds255539.mlab.com:55539/video-dev'}
+  module.exports={mongoURI:'mongodb://knot:11362@ds255539.mlab.com:55539/video-dev'}
 }else{
   module.exports ={mongoURI:'mongodb://localhost/video-dev'}
 }
